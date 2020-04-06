42c9b165711a9717e43e1d83640b3c4961e67bbb
Lesly Juarez <leslyj2@gmail.com>
2020-04-06 15:04:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ def badge_maker(name)
   return "Hello, my name is #{name}"
 end
 
+def batch_badge_creator
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-speakers.each do |name|
-  puts
+
   
